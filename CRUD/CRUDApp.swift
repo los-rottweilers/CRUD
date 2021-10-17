aee11a15a0e4a34f6a6ffbf65d9084cafddf15a4
Andres Cervantes <acervantes107@gmail.com>
2021-10-16 21:52:43 -0500

    Initial Commit

---
+++ CRUD/CRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUDApp.swift
+//  CRUD
+//
+//  Created by andres cervantes on 10/16/21.
+//
+
+import SwiftUI
+
+@main
+struct CRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
